7e03542abaa124373c5c7ead9eac3a705bde3109
Som <aishwarysrivastava2006@gmail.com>
2023-01-27 13:46:35 +0530

    Fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Alert from "./components/Alert";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light"); 
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
     setAlert({
